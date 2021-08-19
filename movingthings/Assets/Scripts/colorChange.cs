1cc030d43fa657569b176e4fd53af748727f2f42
110719008 <you@example.com>
2021-08-20 02:02:12 +0800

    color door

---
+++ movingthings/Assets/Scripts/colorChange.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorChange : MonoBehaviour
+{
+
+    
+
+    public GameObject ObBeChanged;
+    public Color changingColor;
+    private bool playerTouched;
+
+    void Start()
+    {
+        playerTouched = false;
+        
+    }
+    
+    void Update()
+    {
+        if (playerTouched==true)
+        {
+
+            var cubeRenderer = ObBeChanged.GetComponent<Renderer>();
+            cubeRenderer.material.SetColor("_Color", changingColor);
+
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag=="Player")
+        {
+            playerTouched = true;
+        }
+
+    }
+}
